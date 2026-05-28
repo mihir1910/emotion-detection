3af3ee6c14f4d1189c8140c738814bd51db30aed
mihir1910 <mihirraj715@gmail.com>
2026-05-29 00:29:34 +0530

    add tfidf

--- src/model/model_building.py
+++ src/model/model_building.py
@@ -11,6 +11,7 @@ X_train=train_data.iloc[:,0:-1].values
 y_train=train_data.iloc[:,-1].values
 
 clf= GradientBoostingClassifier(n_estimators=50)
+
 clf.fit(X_train,y_train)
 
 pickle.dump(clf, open('models/model.pkl', 'wb'))
